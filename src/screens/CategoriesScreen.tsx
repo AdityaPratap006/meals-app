354be291be10be0255d5c721c0e79957f314f403
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-01 23:59:12 +0530

    padding id

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -24,6 +24,7 @@ const CategoriesScreen = ({ navigation }: CategoriesScreenProp) => {
                 navigation.navigate({
                     routeName: 'CategoryMeals',
                     params: {
+                        categoryId: item.id,
                         categoryTitle: item.title,
                     }
                 });
