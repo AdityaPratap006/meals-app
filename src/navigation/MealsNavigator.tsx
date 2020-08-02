065c0551bca5944f4cd1264fccc4ef8dbbcfec41
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-02 19:29:30 +0530

    Expirmented with material bottom tabs

--- src/navigation/MealsNavigator.tsx
+++ src/navigation/MealsNavigator.tsx
@@ -1,8 +1,10 @@
-import React from 'react'; 
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+// import { Platform } from 'react-native';
+import { createAppContainer, NavigationRouteConfigMap, NavigationRoute, NavigationParams } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator, NavigationBottomTabOptions, NavigationTabProp } from 'react-navigation-tabs';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+// import { createMaterialBottomTabNavigator, NavigationMaterialBottomTabOptions } from 'react-navigation-material-bottom-tabs';
 
 // Constants
 import { colors } from '../constants';
@@ -40,45 +42,61 @@ const MealsStackNavigator = createStackNavigator(
     }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator(
-    {
-        Meals: {
-            screen: MealsStackNavigator,
-            navigationOptions: {
-                tabBarLabel: "All Meals",
-                tabBarIcon: (tabInfo) => {
-                    return (
-                        <Ionicons
-                            name="ios-restaurant"
-                            size={25}
-                            color={tabInfo.tintColor}
-                        />
-                    );
-                },
-            },
-        },
-        Favorites: {
-            screen: FavoritesScreen,
-            navigationOptions: {
-                tabBarLabel: "Favorites!",
-                tabBarIcon: (tabInfo) => {
-                    return (
-                        <Ionicons
-                            name="ios-star"
-                            size={25}
-                            color={tabInfo.tintColor}
-                        />
-                    );
-                },
+let tabScreensConfig: NavigationRouteConfigMap<NavigationBottomTabOptions, NavigationTabProp<NavigationRoute<NavigationParams>, any>, unknown>;
+
+tabScreensConfig = {
+    Meals: {
+        screen: MealsStackNavigator,
+        navigationOptions: {
+            tabBarLabel: "Meals",
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <MaterialIcons
+                        name="restaurant"
+                        size={25}
+                        color={tabInfo.tintColor} />
+                );
             },
         },
     },
+    Favorites: {
+        screen: FavoritesScreen,
+        navigationOptions: {
+            tabBarLabel: "Favorites",
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons
+                        name="ios-star"
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
+            },
+        },
+    },
+}
+
+// colored bottom navigation
+// const materialBottomTabNavigator = createMaterialBottomTabNavigator(
+//     tabScreensConfig,
+//     {
+//        activeColor: 'white',
+//        inactiveColor: '#eee',
+//        shifting: true,
+//     }
+// );
+
+// white bottom navigation
+const bottomTabNavigator = createBottomTabNavigator(
+    tabScreensConfig,
     {
         tabBarOptions: {
             activeBackgroundColor: 'white',
             activeTintColor: colors.primaryColor,
         },
     }
-);
+)
+
+const MealsFavTabNavigator = bottomTabNavigator;
 
 export default createAppContainer(MealsFavTabNavigator);
