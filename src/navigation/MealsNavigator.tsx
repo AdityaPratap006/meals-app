f7ea0d9bd339072c9b7c38422dd85371cd0001f6
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-02 18:13:33 +0530

    Added icons

--- src/navigation/MealsNavigator.tsx
+++ src/navigation/MealsNavigator.tsx
@@ -1,6 +1,8 @@
+import React from 'react'; 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 // Constants
 import { colors } from '../constants';
@@ -11,38 +13,72 @@ import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 
-const MealsStackNavigator = createStackNavigator({
-    Categories: {
-        screen: CategoriesScreen,
-    },
-    CategoryMeals: {
-        screen: CategoryMealsScreen,
-    },
-    MealDetail: {
-        screen: MealDetailScreen,
-    },
-}, {
-    initialRouteName: "Categories",
-    defaultNavigationOptions: {
-        headerTitleStyle: {
-            fontFamily: 'open-sans-bold',
+const MealsStackNavigator = createStackNavigator(
+    {
+        Categories: {
+            screen: CategoriesScreen,
         },
-        headerStyle: {
-            backgroundColor: colors.primaryColor,
-            elevation: 5,
+        CategoryMeals: {
+            screen: CategoryMealsScreen,
         },
-        headerTintColor: "white",
+        MealDetail: {
+            screen: MealDetailScreen,
+        },
     },
-});
+    {
+        initialRouteName: "Categories",
+        defaultNavigationOptions: {
+            headerTitleStyle: {
+                fontFamily: 'open-sans-bold',
+            },
+            headerStyle: {
+                backgroundColor: colors.primaryColor,
+                elevation: 5,
+            },
+            headerTintColor: "white",
+        },
+    }
+);
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: {
-        screen: MealsStackNavigator,
+const MealsFavTabNavigator = createBottomTabNavigator(
+    {
+        Meals: {
+            screen: MealsStackNavigator,
+            navigationOptions: {
+                tabBarLabel: "All Meals",
+                tabBarIcon: (tabInfo) => {
+                    return (
+                        <Ionicons
+                            name="ios-restaurant"
+                            size={25}
+                            color={tabInfo.tintColor}
+                        />
+                    );
+                },
+            },
+        },
+        Favorites: {
+            screen: FavoritesScreen,
+            navigationOptions: {
+                tabBarLabel: "Favorites!",
+                tabBarIcon: (tabInfo) => {
+                    return (
+                        <Ionicons
+                            name="ios-star"
+                            size={25}
+                            color={tabInfo.tintColor}
+                        />
+                    );
+                },
+            },
+        },
     },
-    Favorites: {
-        screen: FavoritesScreen,
-    },
-});
+    {
+        tabBarOptions: {
+            activeBackgroundColor: 'white',
+            activeTintColor: colors.primaryColor,
+        },
+    }
+);
 
 export default createAppContainer(MealsFavTabNavigator);
-  
