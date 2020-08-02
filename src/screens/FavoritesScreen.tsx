3bcc7445e885dbf16517bcabef154ff4c395f7af
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-02 23:42:55 +0530

    Added drawer navigation

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationStackOptions, NavigationStackScreenProps } from 'react-navigation-stack';
+import { NavigationBottomTabOptions } from 'react-navigation-tabs';
+import { NavigationDrawerScreenProps } from 'react-navigation-drawer';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 // Data
 import { MEALS } from '../data/dummyData';
 
 // Components
 import MealCardList from '../components/MealCardList';
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
-const FavoritesScreen = ({ navigation }: NavigationStackScreenProps) => {
+type nestedNavigationScreenProp = NavigationStackScreenProps & NavigationBottomTabOptions & NavigationDrawerScreenProps;
+
+const FavoritesScreen = (props: NavigationStackScreenProps) => {
     const favMeals = MEALS.filter(meal => meal.categoryIds.includes('c1') || meal.categoryIds.includes('c8'));
+
     return (
         <View style={styles.screen}>
             <MealCardList
                 mealList={favMeals}
-                navigation={navigation}
+                navigation={(props as nestedNavigationScreenProp).navigation}
             />
         </View>
     );
@@ -22,9 +29,20 @@ const FavoritesScreen = ({ navigation }: NavigationStackScreenProps) => {
 
 export default FavoritesScreen;
 
-const navOptions: NavigationStackOptions = {
+const navOptions = (navData: NavigationStackScreenProps): NavigationStackOptions => ({
     headerTitle: 'Your Favorites',
-}
+    headerLeft: (props) => (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item
+                title="Menu"
+                iconName="md-menu"
+                onPress={() => {
+                    (navData as nestedNavigationScreenProp).navigation.toggleDrawer();
+                }}
+            />
+        </HeaderButtons>
+    ),
+});
 
 FavoritesScreen.navigationOptions = navOptions;
 
