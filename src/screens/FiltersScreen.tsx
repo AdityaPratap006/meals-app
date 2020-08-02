2a0f82f51bb0613a66a630c019df3edc5b6c5d67
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-02 23:47:09 +0530

    Added drawer navigation

--- src/screens/FiltersScreen.tsx
+++ src/screens/FiltersScreen.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationStackScreenProps, NavigationStackOptions } from 'react-navigation-stack';
+import { NavigationStackOptions, NavigationStackScreenProps } from 'react-navigation-stack';
+import { NavigationBottomTabOptions } from 'react-navigation-tabs';
+import { NavigationDrawerScreenProps } from 'react-navigation-drawer';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const FiltersScreen = ({ navigation }: NavigationStackScreenProps) => {
+// Components
+import CustomHeaderButton from '../components/CustomHeaderButton';
+
+type nestedNavigationScreenProp = NavigationStackScreenProps & NavigationBottomTabOptions & NavigationDrawerScreenProps;
+
+const FiltersScreen = (props: NavigationStackScreenProps) => {
     return (
         <View style={styles.screen}>
             <Text>Filters Screen</Text>
@@ -12,9 +20,20 @@ const FiltersScreen = ({ navigation }: NavigationStackScreenProps) => {
 
 export default FiltersScreen;
 
-const navOptions: NavigationStackOptions = {
+const navOptions = (navData: NavigationStackScreenProps): NavigationStackOptions => ({
     headerTitle: 'Filters',
-}
+    headerLeft: (props) => (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item
+                title="Menu"
+                iconName="md-menu"
+                onPress={() => {
+                    (navData as nestedNavigationScreenProp).navigation.toggleDrawer();
+                }}
+            />
+        </HeaderButtons>
+    ),
+})
 
 FiltersScreen.navigationOptions = navOptions;
 
