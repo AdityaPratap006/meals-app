6bbef6ea969f408f81bea07a2bd2fb134d65af09
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-04 23:17:43 +0530

    Completed FiltersScreen

--- src/screens/FiltersScreen.tsx
+++ src/screens/FiltersScreen.tsx
@@ -1,19 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
 import { NavigationStackOptions, NavigationStackScreenProps } from 'react-navigation-stack';
 import { NavigationBottomTabOptions } from 'react-navigation-tabs';
 import { NavigationDrawerScreenProps } from 'react-navigation-drawer';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+// Constants
+import { colors } from '../constants';
+
 // Components
 import CustomHeaderButton from '../components/CustomHeaderButton';
+
+interface FilrerSwitchProp {
+    state: boolean;
+    setState: (val: boolean) => void;
+    title: string;
+};
+
+const FilterSwitch = (props: FilrerSwitchProp) => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.title}</Text>
+            <Switch
+                trackColor={{
+                    true: colors.primaryColor,
+                    false: '#ccc',
+                }}
+                thumbColor={props.state ? colors.primaryColor : '#ccc'}
+                value={props.state}
+                onValueChange={(newValue) => {
+                    props.setState(newValue);
+                }}
+            />
+        </View>
+    );
+};
 
 type nestedNavigationScreenProp = NavigationStackScreenProps & NavigationBottomTabOptions & NavigationDrawerScreenProps;
 
 const FiltersScreen = (props: NavigationStackScreenProps) => {
+
+    const [isGlutenFree, setIsGlutenFree] = useState<boolean>(false);
+    const [isVegetarian, setIsVegetarian] = useState<boolean>(false);
+    const [isVegan, setIsVegan] = useState<boolean>(false);
+    const [isLactoseFree, setIsLactoseFree] = useState<boolean>(false);
+
     return (
         <View style={styles.screen}>
-            <Text>Filters Screen</Text>
+            <Text style={styles.title}>Filters Screen</Text>
+            <FilterSwitch
+                title='Gluten Free'
+                state={isGlutenFree}
+                setState={setIsGlutenFree}
+            />
+            <FilterSwitch
+                title='Vegetarian'
+                state={isVegetarian}
+                setState={setIsVegetarian}
+            />
+            <FilterSwitch
+                title='Vegan'
+                state={isVegan}
+                setState={setIsVegan}
+            />
+            <FilterSwitch
+                title='Lactose Free'
+                state={isLactoseFree}
+                setState={setIsLactoseFree}
+            />
         </View>
     );
 };
@@ -33,15 +87,28 @@ const navOptions = (navData: NavigationStackScreenProps): NavigationStackOptions
             />
         </HeaderButtons>
     ),
-})
+});
 
 FiltersScreen.navigationOptions = navOptions;
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
     },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center',
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        marginVertical: 10,
+    },
+
 });
